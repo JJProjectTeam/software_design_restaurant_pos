5eb45d88bd22326e48ca129a5a7d38aafb155521
jeancarson <jeanloucarson@gmail.com>
2025-02-24 19:37:15 +0000

    end game frontend only prototype

--- src/main/java/com/softwaredesign/project/view/EndOfGameView.java
+++ src/main/java/com/softwaredesign/project/view/EndOfGameView.java
@@ -1,13 +1,33 @@
 package com.softwaredesign.project.view;
 
+import jexer.TTableWidget;
 
 public class EndOfGameView extends GeneralView{
+    private TTableWidget statsTable;
+
     public EndOfGameView(RestaurantApplication app) {
         super(app);
     }
 
     @Override
     protected void setupView() {
-        window.addLabel("Game Over", 2, 6);
+        window.addLabel("Game Over", 2, 2);
+        statsTable = window.addTable(3, 5, 100, 8, 2, 3);
+        
+        // Set row label width to accommodate longest label
+        
+        statsTable.setColumnLabel(0, "");
+        statsTable.setColumnLabel(1, "Results");
+
+
+        statsTable.setCellText(0,0, "No Customers Served");
+        statsTable.setCellText(0, 1, "No Meals Delivered");
+        statsTable.setCellText(0, 2, "Profit ($)");
+
+        statsTable.setShowRowLabels(false);
+
+        statsTable.setColumnWidth(0, 20);
     }
+    
+
 }
