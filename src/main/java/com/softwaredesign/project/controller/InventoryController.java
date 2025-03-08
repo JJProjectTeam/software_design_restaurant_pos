91444276349d0374bb11949591622bab5050cd1a
jeancarson <jeanloucarson@gmail.com>
2025-03-08 22:47:03 +0000

    more controllers

--- src/main/java/com/softwaredesign/project/controller/InventoryController.java
+++ src/main/java/com/softwaredesign/project/controller/InventoryController.java
@@ -2,25 +2,51 @@ package com.softwaredesign.project.controller;
 
 import com.softwaredesign.project.mediator.RestaurantViewMediator;
 import com.softwaredesign.project.view.ConfigurableView;
+import com.softwaredesign.project.view.InventoryView;
 import com.softwaredesign.project.view.View;
 import java.util.*;
 
 public class InventoryController extends BaseController {
     private Map<String, Integer> ingredients;
+    private Map<String, Double> prices;
+    private RestaurantViewMediator mediator;
     
     public InventoryController() {
         super("Inventory");
         this.ingredients = new HashMap<>();
+        this.prices = new HashMap<>();
+        this.mediator = RestaurantViewMediator.getInstance();
         mediator.registerController("Inventory", this);
+        
+        // Initialize with some default ingredients
+        initializeDefaultIngredients();
     }
     
-    public void addIngredient(String name, int quantity) {
+    private void initializeDefaultIngredients() {
+        // Add some default ingredients with prices
+        addIngredient("Beef Patty", 2.50, 20);
+        addIngredient("Bun", 0.50, 30);
+        addIngredient("Lettuce", 0.25, 40);
+        addIngredient("Tomato", 0.30, 25);
+        addIngredient("Cheese", 0.75, 30);
+        addIngredient("Lamb", 3.00, 15);
+        addIngredient("Pita Bread", 0.60, 25);
+        addIngredient("Onion", 0.20, 35);
+        addIngredient("Tzatziki", 1.00, 20);
+    }
+    
+    public void addIngredient(String name, double price, int quantity) {
         ingredients.put(name, quantity);
-        updateView();
+        prices.put(name, price);
+        notifyIngredientUpdate(name);
     }
     
     public int getIngredientQuantity(String name) {
         return ingredients.getOrDefault(name, 0);
+    }
+    
+    public double getIngredientPrice(String name) {
+        return prices.getOrDefault(name, 0.0);
     }
     
     public Map<String, Integer> getIngredients() {
@@ -39,16 +65,43 @@ public class InventoryController extends BaseController {
         ingredients.put(name, newQuantity);
         System.out.println("[InventoryController] Updated " + name + " quantity to " + newQuantity);
         
-        // Notify inventory views
-        for (View view : mediator.getViews("Inventory")) {
-            if (view instanceof ConfigurableView) {
-                ((ConfigurableView) view).onUpdate(this);
+        notifyIngredientUpdate(name);
+    }
+    
+    private void notifyIngredientUpdate(String ingredientName) {
+        System.out.println("[InventoryController] Notifying views of update for ingredient: " + ingredientName);
+        
+        int quantity = ingredients.getOrDefault(ingredientName, 0);
+        double price = prices.getOrDefault(ingredientName, 0.0);
+        
+        // Update all registered views
+        List<View> views = mediator.getViews("Inventory");
+        for (View view : views) {
+            if (view instanceof InventoryView) {
+                ((InventoryView) view).onInventoryUpdate(
+                    ingredientName,
+                    price,
+                    quantity
+                );
             }
         }
     }
     
     @Override
     public void updateView() {
-        mediator.notifyViewUpdate("Inventory");
+        System.out.println("[InventoryController] Updating all inventory views");
+        
+        // Update all ingredients
+        for (String ingredient : ingredients.keySet()) {
+            notifyIngredientUpdate(ingredient);
+        }
+    }
+    
+    /**
+     * Refresh all ingredients in the view
+     */
+    public void refreshAllIngredients() {
+        System.out.println("[InventoryController] Refreshing all ingredients");
+        updateView();
     }
 }
--- src/main/java/com/softwaredesign/project/controller/KitchenController.java
+++ src/main/java/com/softwaredesign/project/controller/KitchenController.java
@@ -2,55 +2,100 @@ package com.softwaredesign.project.controller;
 
 import com.softwaredesign.project.kitchen.Kitchen;
 import com.softwaredesign.project.kitchen.StationType;
-import com.softwaredesign.project.view.GeneralView;
+import com.softwaredesign.project.kitchen.Station;
+import com.softwaredesign.project.mediator.RestaurantViewMediator;
 import com.softwaredesign.project.view.KitchenView;
+import com.softwaredesign.project.view.View;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class KitchenController extends BaseController {
     private Kitchen kitchen;
+    private RestaurantViewMediator mediator;
+    private Map<StationType, Integer> stationIdMap;
 
     public KitchenController(Kitchen kitchen) {
         super("Kitchen");
         System.out.println("[KitchenController] Initializing controller...");
         this.kitchen = kitchen;
+        this.mediator = RestaurantViewMediator.getInstance();
+        this.stationIdMap = new HashMap<>();
+        
+        // Initialize station IDs
+        int id = 1;
+        for (StationType type : StationType.values()) {
+            stationIdMap.put(type, id++);
+        }
+        
+        // Register with mediator
+        mediator.registerController("Kitchen", this);
     }
 
     @Override
     public void updateView() {
         System.out.println("[KitchenController] Updating all station views");
-        for (StationType station : StationType.values()) {
-            updateRow(station);
+        for (StationType stationType : StationType.values()) {
+            updateStationView(stationType);
         }
     }
 
-    private void updateRow(StationType station) {
+    private void updateStationView(StationType stationType) {
         // Get station data from model
-        // int backlog = kitchen.getBacklog(station);
-        // String assignedChef = kitchen.getAssignedChef(station);
-        // boolean isInUse = kitchen.isStationInUse(station);
-
-        // Convert data for view
-        String stationName = station.toString();
-        // char inUseIndicator = isInUse ? '*' : ' ';
-
-        // System.out.println("[KitchenController] Updating station " + stationName + 
-        //                  " (backlog: " + backlog + 
-        //                  ", chef: " + assignedChef + 
-        //                  ", inUse: " + inUseIndicator + ")");
-
+        List<Station> stations = kitchen.getStationManager().getStationsByType(stationType);
+        if (stations.isEmpty()) {
+            System.out.println("[KitchenController] No stations found for type: " + stationType);
+            return;
+        }
+        
+        // For simplicity, we'll just use the first station of each type
+        Station station = stations.get(0);
+        
+        // Get station data
+        int stationId = stationIdMap.get(stationType);
+        String stationName = stationType.toString();
+        int backlog = station.getBacklogSize();
+        String chefName = station.hasChef() ? station.getAssignedChef().getName() : "None";
+        char inUse = station.isBusy() ? '*' : ' ';
+        
+        System.out.println("[KitchenController] Updating station " + stationName + 
+                         " (ID: " + stationId + 
+                         ", backlog: " + backlog + 
+                         ", chef: " + chefName + 
+                         ", inUse: " + inUse + ")");
+        
         // Update all registered views
-        // for (GeneralView view : mediator.getViews("Kitchen")) {
-        //     if (view instanceof KitchenView) {
-        //         ((KitchenView) view).onStationUpdate(
-        //             stationName,
-        //             backlog,
-        //             assignedChef,
-        //             inUseIndicator
-        //         );
-        //     }
-        //     else {
-        //         System.out.println("[KitchenController] View is not a KitchenView, skipping update");
-        //     }
-        // }
+        List<View> views = mediator.getViews("Kitchen");
+        for (View view : views) {
+            if (view instanceof KitchenView) {
+                ((KitchenView) view).onStationUpdate(
+                    stationId,
+                    stationName,
+                    backlog,
+                    chefName,
+                    inUse
+                );
+            } else {
+                System.out.println("[KitchenController] View is not a KitchenView, skipping update");
+            }
+        }
+    }
+    
+    /**
+     * Notify views about a specific station update
+     */
+    public void notifyStationUpdate(StationType stationType) {
+        System.out.println("[KitchenController] Notifying views of update for station: " + stationType);
+        updateStationView(stationType);
+    }
+    
+    /**
+     * Refresh all stations in the view
+     */
+    public void refreshAllStations() {
+        System.out.println("[KitchenController] Refreshing all stations");
+        updateView();
     }
 
     public Kitchen getKitchen() {
